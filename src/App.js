463bddba9f9479ce7587de2809fd22cf9c1ef0d6
babinicz <babinicz@onet.pl>
2022-11-16 19:49:07 +0100

    zmiany

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import "./scss/main.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CookieConsent from "react-cookie-consent";
 
 const App = () => {
   return (
@@ -15,6 +16,33 @@ const App = () => {
         </Routes>
         <Footer />
       </Router>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </div>
   );
 };
